8f152e92f55795174a92716ee0887738dc2c6d03
John Hervey <johnhervey@Johns-MacBook-Pro.local>
2012-07-29 17:35:40 +1200

    Initial Commit

---
+++ NecleoAlign/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NecleoAlign
+//
+//  Created by John Hervey on 29/07/12.
+//  Copyright (c) 2012 John Hervey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
